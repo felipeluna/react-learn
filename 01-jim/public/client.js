a3be4ddf06b9b0e5b55d524152cc1d651b3ffca9
Felipe Luna <pancreasnou@gmail.com>
2016-08-01 10:19:58 -0300

    unique elements with for loop

--- 01-jim/public/client.js
+++ 01-jim/public/client.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-var videos = require("./jim.js")
-
+import videos from "./jim.js";
 
 class MovieSection extends React.Component {
 	render(){
+	var data = videos.allVideos();
 		return (
 			<div className="movieSection">
-				<MovieList />
+				<MovieList data={data} />
 			</div>
 		);
 	}
@@ -15,10 +15,10 @@ class MovieSection extends React.Component {
 
 class MovieList extends React.Component {
 	render(){
-		var a = videos.allVideos();
+		var a = this.props.data;
 		var rows = [];
-		for (var i = 0; i < a.length; i++) {	
-			rows.push(<Video video={a[i]} />)
+		for (var i = 0; i <a.length; i++) {	
+			rows.push(<Video  key={i} link={a[i].link} thumb={a[i].thumb} title={a[i].title} />)
 		}
 		return(
 			<div className="movieList">
@@ -32,11 +32,11 @@ class Video extends React.Component {
 	render(){
 		return(
 			<div className="video">
-				<a href={this.props.video.link}>
-					<img src={this.props.video.thumb} alt=""/>
+				<a href={this.props.link}>
+					<img src={this.props.thumb} alt={this.props.title}/>
 				</a>
 				<br/>
-				{this.props.video.title}
+				{this.props.title}
 			</div>
 		);
 	}
